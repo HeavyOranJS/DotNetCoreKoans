3a59899c6b7d02066da9970eb3861ee655419ea4
HeavyOranJS <whereorange@gmail.com>
2020-11-11 12:13:04 +1000

    aboutConstants

--- Koans/AboutConstants.cs
+++ Koans/AboutConstants.cs
@@ -9,7 +9,7 @@ namespace DotNetCoreKoans.Koans
         public void ConstantsMustBeInitalizedAsDeclared()
         {
             const int months = 12;
-            Assert.Equal(FILL_ME_IN, 12);
+            Assert.Equal(months, 12);
         }
 
         [Step(2)]
@@ -19,8 +19,9 @@ namespace DotNetCoreKoans.Koans
             //code, you will not achieve zen when attempting
             //to change them after definition.
             const int days = 365;
-            //days = days + 1; //
-            Assert.Equal(FILL_ME_IN, 365);
+            // days = days + 1; //does not even compile -- gives CS0131
+            // also this means compile errors and runtime exceptions are different things
+            Assert.Equal(365, days);
         }
 
         [Step(3)]
@@ -29,9 +30,9 @@ namespace DotNetCoreKoans.Koans
             //You can achieve zen (and save keystrokes) by defining
             //constants of the same type as one.
             const int months = 12, weeks = 52, days = 365;
-            Assert.Equal(typeof(FillMeIn), months.GetType());
-            Assert.Equal(typeof(FillMeIn), weeks.GetType());
-            Assert.Equal(typeof(FillMeIn), days.GetType());
+            Assert.Equal(typeof(int), months.GetType());
+            Assert.Equal(typeof(int), weeks.GetType());
+            Assert.Equal(typeof(int), days.GetType());
         }
 
         [Step(4)]
@@ -43,28 +44,22 @@ namespace DotNetCoreKoans.Koans
 
             const double daysPerWeek = (double)days / (double)weeks;
             const double daysPerMonth = (double)days / (double)months;
-            Assert.Equal(FILL_ME_IN, daysPerWeek);
-            Assert.Equal(FILL_ME_IN, daysPerMonth);
+            Assert.Equal(7.019230769230769d, daysPerWeek);
+            Assert.Equal(30.416666666666668d, daysPerMonth);
 
             //Constants can be used in arithmetic to set other constant values.
             //They can also initialize each other.
         }
 
-        class Animal
+        public class Animal
         {
             public const int Legs = 4;
 
-            public int LegsInAnimal()
-            {
-                return Legs;
-            }
+            public int LegsInAnimal() { return Legs; }
 
             public class NestedAnimal
             {
-                public int LegsInNestedAnimal()
-                {
-                    return Legs;
-                }
+                public int LegsInNestedAnimal() { return Legs; }
             }
         }
 
@@ -72,12 +67,21 @@ namespace DotNetCoreKoans.Koans
         public void NestedClassesInheritConstantsFromEnclosingClasses()
         {
             var nestedAnimal = new Animal.NestedAnimal();
-            Assert.Equal(FILL_ME_IN, nestedAnimal.LegsInNestedAnimal());
+            Assert.Equal(Animal.Legs, nestedAnimal.LegsInNestedAnimal());
 
             //QUESTION: Do nested classes inherit their parent's scope?
+
+            var animal = new Animal();
+            // you cannot get to public consts from class insatnce
+            // there needs to be a getter -- LegsInAnimal in this case
+            Assert.Equal(Animal.Legs, animal.LegsInAnimal());
+            Assert.Equal(nestedAnimal.LegsInNestedAnimal(), animal.LegsInAnimal());
+            // but this does not work: NestedAnimal does not have access
+            // public const of inclusive class -- it's out of scope
+            // Assert.Equal(Animal.NestedAnimal.Legs, animal.LegsInAnimal());
         }
 
-        class Reptile : Animal
+        public class Reptile : Animal
         {
             public int LegsInReptile()
             {
@@ -91,10 +95,12 @@ namespace DotNetCoreKoans.Koans
             //If a Reptile is an Animal, zen is achieved
             //when you realize they too will have legs.
             var reptile = new Reptile();
-            Assert.Equal(FILL_ME_IN, reptile.LegsInReptile());
+            Assert.Equal(Animal.Legs, reptile.LegsInReptile());
+            // okay, okay, but what about Reptile.Legs -- works, great
+            Assert.Equal(Reptile.Legs, reptile.LegsInReptile());
         }
 
-        class MyAnimals
+        public class MyAnimals
         {
             public const int Legs = 2;
 
@@ -111,7 +117,9 @@ namespace DotNetCoreKoans.Koans
         public void WhoWinsWithBothNestedAndInheritedConstants()
         {
             var bird = new MyAnimals.Bird();
-            Assert.Equal(FILL_ME_IN, bird.LegsInBird());
+            Assert.Equal(Animal.Legs, bird.LegsInBird());
+
+            // i mean this koan is great, but not about constans :D
 
             /* QUESTION: Which has precedence: The constant in the lexical scope,
                or the constant from the inheritance hierarchy? */
